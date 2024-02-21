87b0d0379a315067e021c29dd015534df472557e
z0eh <147090783+z0eh@users.noreply.github.com>
2024-02-21 17:31:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "dnd" #online/dnd/idle
 
-custom_status = "youtube.com/@SealedSaucer" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+custom_status = "" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
